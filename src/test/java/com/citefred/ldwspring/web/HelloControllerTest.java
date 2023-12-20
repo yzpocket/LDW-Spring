a528c9d0b6eadf152a5746abbaf804a31d20c430
INYONG KIM <ohnyong87@gmail.com>
2023-12-20 17:03:15 +0900

    test : 간단한 Dto 생성 및 롬복 테스트 코드 작성

--- src/test/java/com/citefred/ldwspring/web/HelloControllerTest.java
+++ src/test/java/com/citefred/ldwspring/web/HelloControllerTest.java
@@ -1,14 +1,17 @@
 package com.citefred.ldwspring.web;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = HelloController.class)
 public class HelloControllerTest {
 
---
+++ src/test/java/com/citefred/ldwspring/web/HelloResponseDtoTest.java
@@ -0,0 +1,24 @@
+package com.citefred.ldwspring.web;
+
+import com.citefred.ldwspring.web.dto.HelloResponseDto;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void 롬복_기능_테스트() {
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto helloResponseDto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(helloResponseDto.getName()).isEqualTo(name);
+        assertThat(helloResponseDto.getAmount()).isEqualTo(amount);
+
+    }
+}
