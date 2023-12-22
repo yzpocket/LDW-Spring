b3792dbf05f2370a58d6c8e64efb100f4d8002da
INYONG KIM <ohnyong87@gmail.com>
2023-12-22 21:56:40 +0900

    feat : 게시글 등록 API 작성

---
+++ src/main/java/com/citefred/ldwspring/service/PostsService.java
@@ -0,0 +1,18 @@
+package com.citefred.ldwspring.service;
+
+import com.citefred.ldwspring.domain.posts.PostsRepository;
+import com.citefred.ldwspring.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
