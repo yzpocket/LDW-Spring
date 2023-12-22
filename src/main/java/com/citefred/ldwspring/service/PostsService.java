4db3e2b57533b3b35466e85cd4a407a184e3868d
INYONG KIM <ohnyong87@gmail.com>
2023-12-22 23:24:53 +0900

    feat : 게시글 수정, 조회 API 코드 작성

--- src/main/java/com/citefred/ldwspring/service/PostsService.java
+++ src/main/java/com/citefred/ldwspring/service/PostsService.java
@@ -1,7 +1,10 @@
 package com.citefred.ldwspring.service;
 
+import com.citefred.ldwspring.domain.posts.Posts;
 import com.citefred.ldwspring.domain.posts.PostsRepository;
+import com.citefred.ldwspring.web.dto.PostsResponseDto;
 import com.citefred.ldwspring.web.dto.PostsSaveRequestDto;
+import com.citefred.ldwspring.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +17,20 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
     }
 }
