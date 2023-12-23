bc8303f46ea8007ff500bccebcdc5c27fd7caced
INYONG KIM <ohnyong87@gmail.com>
2023-12-23 21:53:05 +0900

    feat : Timestamp기능의 엔터티 작성 및 JPA Auditing 설정

--- src/main/java/com/citefred/ldwspring/domain/posts/Posts.java
+++ src/main/java/com/citefred/ldwspring/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.citefred.ldwspring.domain.posts;
 
+import com.citefred.ldwspring.domain.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,7 +9,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
