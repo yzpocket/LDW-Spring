217436d8ed56d0ad70ffc668cb7a124cff70af6d
INYONG KIM <ohnyong87@gmail.com>
2023-12-26 14:29:54 +0900

    feat : 글 수정 기능 뷰 페이지 생성 및 API 연결 구현

--- src/main/java/com/citefred/ldwspring/web/IndexController.java
+++ src/main/java/com/citefred/ldwspring/web/IndexController.java
@@ -1,10 +1,13 @@
 package com.citefred.ldwspring.web;
 
 import com.citefred.ldwspring.service.PostsService;
+import com.citefred.ldwspring.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RequiredArgsConstructor
 @Controller
@@ -20,5 +23,14 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id,
+                              Model model){
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
