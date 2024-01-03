698fc91f85a35e9a580176a72e514411e7e35442
INYONG KIM <ohnyong87@gmail.com>
2024-01-03 17:37:36 +0900

    chore : LocalDateTime과 관련된 역직렬화 문제 해결

--- src/main/java/com/citefred/ldwspring/domain/BaseTimeEntity.java
+++ src/main/java/com/citefred/ldwspring/domain/BaseTimeEntity.java
@@ -1,5 +1,9 @@
 package com.citefred.ldwspring.domain;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -13,9 +17,14 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     @CreatedDate
     private LocalDateTime createdDate;
 
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     @LastModifiedDate
     private LocalDateTime modifiedDate;
 }
